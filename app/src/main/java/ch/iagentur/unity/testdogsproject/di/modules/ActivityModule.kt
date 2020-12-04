4e6cad296711945d012c22bc8659493142a9ca2a
Andrey T <a.a.tereschenko@gmail.com>
2020-12-04 11:03:22 +0300

    removed unused navigation classes

--- app/src/main/java/ch/iagentur/unity/testdogsproject/di/modules/ActivityModule.kt
+++ app/src/main/java/ch/iagentur/unity/testdogsproject/di/modules/ActivityModule.kt
@@ -2,7 +2,6 @@ package ch.iagentur.unity.testdogsproject.di.modules
 
 import androidx.appcompat.app.AppCompatActivity
 import ch.iagentur.unity.testdogsproject.di.PerActivity
-import ch.iagentur.unity.testdogsproject.ui.navigation.MainFragmentNavigator
 import dagger.Module
 import dagger.Provides
 
@@ -13,11 +12,5 @@ class ActivityModule constructor(private val activity: AppCompatActivity) {
     @PerActivity
     fun provideActivity(): AppCompatActivity {
         return activity
-    }
-
-    @Provides
-    @PerActivity
-    fun provideNavigator(): MainFragmentNavigator {
-        return MainFragmentNavigator(activity.supportFragmentManager)
     }
 }
--- app/src/main/java/ch/iagentur/unity/testdogsproject/ui/screens/main/MainActivity.kt
+++ app/src/main/java/ch/iagentur/unity/testdogsproject/ui/screens/main/MainActivity.kt
@@ -1,35 +1,15 @@
 package ch.iagentur.unity.testdogsproject.ui.screens.main
 
 import android.os.Bundle
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import ch.iagentur.unity.testdogsproject.R
-import ch.iagentur.unity.testdogsproject.ui.navigation.MainFragmentNavigator
 import ch.iagentur.unity.testdogsproject.ui.screens.base.BaseActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
-    @Inject
-    lateinit var mainScreenNavigator: MainFragmentNavigator
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         activityComponent?.inject(this)
         Navigation.createNavigateOnClickListener(R.id.nav_host_fragment)
     }
-
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        mainScreenNavigator.onSaveState(outState)
-//    }
-
-//    override fun onBackPressed() {
-//        if (!mainScreenNavigator.onBackNavigation()) {
-//            super.onBackPressed()
-//        }
-//    }
 }
