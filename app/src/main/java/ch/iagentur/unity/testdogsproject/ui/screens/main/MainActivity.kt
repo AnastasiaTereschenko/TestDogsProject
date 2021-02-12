32903f637ed16e9269da1927a0ef6de409a7545b
Andrey T <a.a.tereschenko@gmail.com>
2021-02-12 15:16:18 +0300

    changed open fragment on main activity

--- app/src/main/java/ch/iagentur/unity/testdogsproject/ui/screens/main/MainActivity.kt
+++ app/src/main/java/ch/iagentur/unity/testdogsproject/ui/screens/main/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         activityComponent?.inject(this)
-        navigator.applyCommands(arrayOf<Command>(Replace(DogBreeds())))
+        router.navigateTo(DogBreeds())
     }
 
     override fun onResumeFragments() {
